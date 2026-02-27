c6e794a805bafbd076f3fd3a7ce53165fa3d1175
Aitzol <aitzoletxaburu0@gmail.com>
2026-02-27 14:39:07 +0100

    Remove database password from configuration
    Removed the database password for security reasons.

--- db_config.php
+++ db_config.php
@@ -3,4 +3,4 @@
 define('DB_HOST', 'database-mysql.c2nyima6e4fq.us-east-1.rds.amazonaws.com');
 define('DB_NAME', 'denda');
 define('DB_USER', 'admin');
-define('DB_PASS', '451680Aee-.');
+define('DB_PASS', '');
